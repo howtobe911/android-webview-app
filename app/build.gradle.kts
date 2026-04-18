4f6794cb95238baf789b53a5fec85649ef292da4
root <root@5725799-cm26335>
2026-04-19 00:08:20 +0300

    Upgrade AGP to 9.0.1, Gradle to 9.1.0, compileSdk 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.second.risedie.challengeapp"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.second.risedie.challengeapp"
